2b3cf5ebed594c81ffad1fa5a87bf47f70023a0f
Vinicius de Carvalho Barboza <vini_000@live.com>
2020-10-11 12:25:33 -0300

    Adicionar perfil na lista e salvando no storage

---
+++ src/routes.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Main from './pages/main';
+import User from './pages/user';
+
+const PagesStack = createStackNavigator();
+
+const PagesStackScreen = () => (
+    <PagesStack.Navigator
+        screenOptions={{
+            headerTitleAlign: 'center',
+            headerStyle: { backgroundColor: '#7159C1' },
+            headerTintColor: '#FFF',
+            headerBackTitleVisible: false,
+        }}
+    >
+        <PagesStack.Screen
+            name="Main"
+            options={{
+                title: 'Usuários',
+            }}
+            component={Main}
+        />
+        <PagesStack.Screen
+            name="User"
+            options={{
+                title: 'Users',
+            }}
+            component={User}
+        />
+    </PagesStack.Navigator>
+);
+
+export default () => (
+    <NavigationContainer>
+        <PagesStackScreen />
+    </NavigationContainer>
+);
